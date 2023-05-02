5cf3aac46f8e077146aa17f44f5a3e661547149d
ntnyq <ntnyq13@gmail.com>
2023-05-02 09:05:01 +0800

    fix: use unocss reset

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const META = Object.freeze({
 })
 
 export const IDENTITIES = Object.freeze({
-  GOOGLE_TAG_MANAGER_ID: `GTM-T497H2P`,
-  GITHUB_USERNAME: `ntnyq`,
-  TWITTER_USERNAME: `ntnyq`,
+  GOOGLE_TAG_MANAGER_ID: 'GTM-T497H2P',
+  GITHUB_USERNAME: 'ntnyq',
+  TWITTER_USERNAME: 'ntnyq',
 })
